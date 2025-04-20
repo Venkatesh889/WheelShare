d9370ced4ff3e944eb3423a77f2a583cf93def5a
Venkatesh <vtummala2@student.touro.edu>
2025-04-19 22:06:59 -0400

    Update

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,16 +1,17 @@
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
-const authenticateUser = (req, res, next) => {
+
+export default function authenticateUser(req, res, next)
+{
     const token = req.header("Authorization");
     if (!token) return res.status(401).json({ message: "Access denied" });
 
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
+
         next();
     } catch (err) {
         res.status(400).json({ message: "Invalid token" });
     }
-};
+}
 
-module.exports = authenticateUser;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,37 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import carRoutes from './routes/carRoutes.js';
+import bookingRoutes from './routes/bookingsRoutes.js';
 
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
+connectDB().then(() => {
+    console.log('MongoDB Connected');
+})
+
+//Middleware
+app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("WheelShare API is running...");
 });
 
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/cars', carRoutes);
+app.use('/api/bookings', bookingRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const authenticateUser = require("./middleware/authMiddleware");
+import authenticateUser from "./middleware/authMiddleware.js";
+
+
 
 app.get("/api/protected", authenticateUser, (req, res) => {
     res.json({ message: "You have accessed a protected route!", user: req.user });
